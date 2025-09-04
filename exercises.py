d1a41a39d9b9399f3099fb8ac97b5fa76f24c415
Dae Dowdell <dow_dell2010@yahoo.com>
2025-09-04 14:12:21 -0400

    exc 2 not working correctly

--- exercises.py
+++ exercises.py
@@ -8,10 +8,12 @@
 # - The list should contain any three elements of your choice.
 # - Use a loop to print each element.
 
+
 def example_list_function():
-  example_list = ['element1', 'element2', 'element3']
-  for element in example_list:
-      print(element)
+    example_list = ['element1', 'element2', 'element3']
+    for element in example_list:
+        print(element)
+
 
 # Call the function and print each element
 example_list_function()
@@ -22,11 +24,31 @@ example_list_function()
 # Assign the second student’s name to a variable named first_student.
 # Assign the last student’s name to a variable named last_student.
 
+
 def manage_students():
-   students = ['John', 'James', 'Jacob']
-   first_student = students[1]
-   last_student = students[-1]
-   return f"First student: {first_student}, Last student: {last_student}"
+    students = ['John', 'James', 'Jacob']
+    first_student = students[1]
+    last_student = students[-1]
+    return f'First student: {first_student}, Last student: {last_student}'
+
 
 # Call the function and print the result
 print('Exercise 1:', manage_students())
+
+# Exercise 2: Loop and String Concatenation
+#
+# Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
+# Create a variable named meal and assign an empty string to it.
+# Use a for loop to iterate over the strings in foods and append each string to meal.
+
+
+def combine_foods():
+    foods = ('pizza', 'tacos', 'cookies')
+    meal = ''
+    for food in foods:
+        meal += food + ' '
+        return meal
+
+
+# Call the function and print the result
+print('Exercise 2:', combine_foods())
